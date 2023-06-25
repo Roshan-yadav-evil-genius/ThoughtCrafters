e84e623affc5e2685124d8b5cd980754add7b705
Roshan-yadav-evil-genius <roshanyadav1st@gmail.com>
2023-06-25 18:19:53 +0530

    refactor(frontend): :art: header, main, footer sections added and Routing  implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Services from './Pages/Services';
+import BaseTemplate from './Pages/BaseTemplate';
 
 function App() {
   return (
     <div className="App">
-      <h1>ThougghtCrafters</h1>
+      <Routes>
+        <Route path="/" element={<BaseTemplate />} >
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
